b4286bd3f7bce04f27a0db7c40616cfa1e70244b
perfwill <perfwill@gmail.com>
2011-04-28 08:20:42 +0700

    support language for the login form

--- application/views/form_login.php
+++ application/views/form_login.php
@@ -3,15 +3,15 @@
 <?=form_open('index/login')?>
 	<?=form_fieldset('')?>
 		<div class='textfield'>
-			<?=form_label('Username: ', 'username')?>
+			<?=form_label($this->lang->line('login_username').': ', 'username')?>
 			<?=form_input('username')?>
 		</div>	
 		<div class='textfield'>
-			<?=form_label('Password: ', 'password')?>
+			<?=form_label($this->lang->line('login_password').': ', 'password')?>
 			<?=form_password('password')?>
 		</div>
 		<div class='checkfield'>
-			<?=form_label('Remember me', 'remember_chk')?>
+			<?=form_label($this->lang->line('login_remember'), 'remember_chk')?>
 			<?=form_checkbox('remember_chk', 'yes', TRUE)?>
 		</div>
 		<div class='buttons'>
